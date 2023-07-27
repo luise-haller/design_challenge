c9afd7b3c5e8b0bc1b53b60409f954688e966085
luise-haller <luise@h4ll3r.net>
2023-07-27 16:09:41 +0000

    Changes from update-test branch to this branch

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -48,6 +48,50 @@ def protect_firmware(infile, outfile, version, message):
     with open(outfile, 'wb+') as outfile:
         outfile.write(firmware_blob)
 
+def verify_firmware(infile):
+    # Load firmware bianry from infile
+    with open(infile, 'rb') as fp:
+        firmware_final = fp.read()
+
+    # Extract metadata, og hash, encrypted hash, HMAC tag
+    metadata = firmware_final[:4]
+    og_hash = firmware_final[-96:-64]
+    encrypted_hash = firmware_final[-64:-32]
+    hmac_tag = firmware_final[-32:]
+
+    # get firmware data without metadata and appended mes
+    firmware = firmware_final[4:-97]
+
+    # Unpack version and size from metadata
+    version, size = struct.unpack('<HH', metadata)
+
+    #Load keys from secret_build_output.txt
+    with open("secret_build_output.txt", "rb") as file:
+        aes_key = file.readline().strip()
+        iv = file.readline().strip()
+        hmac_key = file.readline().strip()
+
+    # Calculate SHA256 hashes and HMAC tag for received firmware
+    recalculated_og_sha = SHA256.new(firmware)
+    recalculated_og_hash = recalculated_og_sha.digest()
+
+    # Decrypt firmware using AES-GCM so as to recalculate encrypted SHA256 hash
+    aes_cipher = AES.new(aes_key, AES.MODE_GCM, nonce=iv)
+    decrypted_firmware = aes_cipher.decrypt(firmware)
+
+    # Recalculate SHA256 hashes and HMAC tag for received firmware
+    recalculated_encrypted_sha = SHA256.new(decrypted_firmware)
+    recalculated_encrypted_hash = recalculated_encrypted_sha.digest()
+
+    # Recalculate HMAC tag
+    hmac_generate = HMAC.new(hmac_key, msg=decrypted_firmware, digestmod=SHA256)
+    recalculated_hmac_tag = hmac_generate.digest()
+
+    # compare the calculated values with the extracted ones to verify firmware
+    if og_hash == recalculated_og_hash and encrypted_hash == recalculated_encrypted_hash and hmac_tag == recalculated_hmac_tag:
+        print("Firmware integrity verified")
+    else:
+        print("Firmware integrity verfication failed")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Firmware Update Tool')
@@ -58,3 +102,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
+    verify_firmware(infile=args.outfile)
