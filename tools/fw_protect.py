9e53f05d863d12a528e031c659fe63bfefe44d80
azhou2056 <azhou2056@gmail.com>
2023-07-27 21:33:49 +0000

    removed SHA, only using mac and HMAC now

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -39,60 +39,48 @@ def protect_firmware(infile, outfile, version, message):
 
     # Generates HMAC of the frame
     hMAC = HMAC.new(hmac, msg=frame, digestmod=SHA256).digest()
-
+    
     # Frame + HMAC + Message + Null Byte
     firmware_blob = frame + hMAC + message.encode() + b'\00'
-
 
     # Write final firmware to outfile
     with open(outfile, 'wb+') as outfile:
         outfile.write(firmware_blob)
 
 def verify_firmware(infile):
-    # Load firmware bianry from infile
+    # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware_final = fp.read()
 
     # Extract metadata, og hash, encrypted hash, HMAC tag
     metadata = firmware_final[:4]
-    og_hash = firmware_final[-96:-64]
-    encrypted_hash = firmware_final[-64:-32]
+    mac_tag = firmware_final[-48:-32]
     hmac_tag = firmware_final[-32:]
 
     # get firmware data without metadata and appended mes
-    firmware = firmware_final[4:-97]
+    firmware = firmware_final[4:-49]
 
-<<<<<<< HEAD
-    # Unpack the version and size from metadata
-=======
     # Unpack version and size from metadata
->>>>>>> 82bfb0fef57ec9f7845e4ba998db2c7fb6b22e11
     version, size = struct.unpack('<HH', metadata)
 
     #Load keys from secret_build_output.txt
     with open("secret_build_output.txt", "rb") as file:
         aes_key = file.readline().strip()
         iv = file.readline().strip()
-        hmac_key = file.readline().strip()
-
-    # Calculate SHA256 hashes and HMAC tag for received firmware
-    recalculated_og_sha = SHA256.new(firmware)
-    recalculated_og_hash = recalculated_og_sha.digest()
+        hmac = file.readline().strip()
 
     # Decrypt firmware using AES-GCM so as to recalculate encrypted SHA256 hash
     aes_cipher = AES.new(aes_key, AES.MODE_GCM, nonce=iv)
     decrypted_firmware = aes_cipher.decrypt(firmware)
-
-    # Recalculate SHA256 hashes and HMAC tag for received firmware
-    recalculated_encrypted_sha = SHA256.new(decrypted_firmware)
-    recalculated_encrypted_hash = recalculated_encrypted_sha.digest()
+    recalculated_mac_tag = aes_cipher.mac()
 
     # Recalculate HMAC tag
-    hmac_generate = HMAC.new(hmac_key, msg=decrypted_firmware, digestmod=SHA256)
+    frame = metadata + firmware + mac_tag
+    hmac_generate = HMAC.new(hmac, msg=frame, digestmod=SHA256)
     recalculated_hmac_tag = hmac_generate.digest()
 
     # compare the calculated values with the extracted ones to verify firmware
-    if og_hash == recalculated_og_hash and encrypted_hash == recalculated_encrypted_hash and hmac_tag == recalculated_hmac_tag:
+    if  mac_tag == recalculated_mac_tag and hmac_tag == recalculated_hmac_tag:
         print("Firmware integrity verified")
     else:
         print("Firmware integrity verfication failed")
@@ -106,8 +94,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
-<<<<<<< HEAD
     verify_firmware(infile=args.infile)
-=======
-    verify_firmware(infile=args.infile)
->>>>>>> 82bfb0fef57ec9f7845e4ba998db2c7fb6b22e11
